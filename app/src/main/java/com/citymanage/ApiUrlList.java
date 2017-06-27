a82c35377170aaf8de5308ac07eab6e0b6da080e
sigjeongjin <jadepoint@naver.com>
2017-06-27 15:18:36 +0900

    파싱 기능 변경

---
+++ app/src/main/java/com/citymanage/ApiUrlList.java
@@ -0,0 +1,13 @@
+package com.citymanage;
+
+/**
+ * Created by we25 on 2017-06-27.
+ */
+
+public class ApiUrlList {
+//    private final static String HOST = "http://192.168.0.230:3000";
+//    private final static String PUSHHISTORY = HOST + "/pushHistory";
+//    private final static String LOGIN = HOST + "/login";
+}
+
+

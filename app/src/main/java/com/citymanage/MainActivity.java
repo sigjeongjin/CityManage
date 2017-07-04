499bb82f05a73de5d668cb549d43a78224088746
sigjeongjin <jadepoint@naver.com>
2017-07-04 17:23:23 +0900

    메뉴 추가 , 로그아웃 준비

--- app/src/main/java/com/citymanage/MainActivity.java
+++ app/src/main/java/com/citymanage/MainActivity.java
@@ -3,6 +3,7 @@ package com.citymanage;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -72,6 +73,35 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
     }
+    //메뉴 생성
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+
+
+
+    }
+
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.logout_bt7:
+    //로그아웃 객체 생성
+//                SharedPrefManager.getInstance(this).logout();
+//                finish();
+
+//                startActivity(new Intent(this, LoginActivity.class));
+//                break;
+//        }
+//           return true;
+//    }
 }
+
+
+
+
 
 

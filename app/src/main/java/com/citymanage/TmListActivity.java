11735d4328f2605df97d54f69bb55f2649781ff5
sigjeongjin <jadepoint@naver.com>
2017-07-06 14:12:34 +0900

    쓰레기통 관리 화면 시작

---
+++ app/src/main/java/com/citymanage/TmListActivity.java
@@ -0,0 +1,161 @@
+package com.citymanage;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class TmListActivity extends AppCompatActivity {
+    EditText addressInput;
+
+    Button wmMapActivityGo;
+    ListView listView;
+    WmAdapter adapter;
+
+    String resultCode;
+    String url = "";
+    ProgressDialog dialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wm_list);
+
+        wmMapActivityGo = (Button) findViewById(R.id.wmMapActivityGo);
+
+        wmMapActivityGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TmMapActivity.class);
+                startActivity(intent);
+                //finish();
+            }
+        });
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        adapter = new WmAdapter();
+
+        adapter.addItem(new WmItem("서울시 금천구", "정상", "정상"));
+        adapter.addItem(new WmItem("서울시 구로구", "비정상", "정상"));
+        adapter.addItem(new WmItem("서울시 관악구", "정상", "비정상"));
+
+
+        listView.setAdapter(adapter);
+
+        addressInput = (EditText) findViewById(R.id.addressInput);
+
+        Button button = (Button) findViewById(R.id.searchBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = addressInput.getText().toString();
+                String mobile = "010-1000-1000";
+
+                //adapter.addItem(new WmItem(name, mobile, age, R.drawable.muji01));
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        // liseView click --> WminfoActivity
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                WmItem item = (WmItem) adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), "선택 : " + item.getAddress(), Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), WmInfoActivity.class);
+
+                intent.putExtra("address", adapter.items.get(position).address);
+                intent.putExtra("waterQuality", adapter.items.get(position).waterQuality);
+                intent.putExtra("waterLevel", adapter.items.get(position).waterLevel);
+
+
+                startActivityForResult(intent, 1001);
+
+            }
+        });
+    }
+
+    class WmAdapter extends BaseAdapter {
+        ArrayList<WmItem> items = new ArrayList<WmItem>();
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        public void addItem(WmItem item) {
+            items.add(item);
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup viewGroup) {
+            //WmItemView view = new WmItemView(getApplicationContext());
+
+            WmItemView view = null;
+
+            if (convertView == null) {
+                view = new WmItemView(getApplicationContext());
+            } else {
+                view = (WmItemView) convertView;
+            }
+
+            WmItem item = items.get(position);
+            view.setAddress(item.getAddress());
+            view.setWaterQuality(item.getWaterQuality());
+            view.setWaterLevel(item.getWaterLevel());
+
+
+            return view;
+        }
+    }
+
+    void parseJsonData(String jsonString) {
+        try {
+            JSONObject object = new JSONObject(url);    // JSONObject에 객체를 저장
+
+            Log.d("RESULTCODE", "ListTest");
+
+            resultCode = object.getString("resultCode");
+
+
+//            JSONObject object = new JSONObject(jsonString);
+//            JSONArray fruitsArray = object.getJSONArray("fruits");
+//            ArrayList al = new ArrayList();
+//
+//            for(int i = 0; i < fruitsArray.length(); ++i) {
+//                al.add(fruitsArray.getString(i));
+//            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        dialog.dismiss();
+    }
+
+}

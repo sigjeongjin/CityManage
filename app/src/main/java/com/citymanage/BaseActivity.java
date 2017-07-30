12a6fe560485a7ae6c790c3c66ddb3bcbcc436a8
sigjeonjin <jadepoint@naver.com>
2017-07-30 10:56:31 +0900

    톰캣 아이피 추가

--- app/src/main/java/com/citymanage/BaseActivity.java
+++ app/src/main/java/com/citymanage/BaseActivity.java
@@ -11,9 +11,10 @@ import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
 
-//    public static final  String HOST = "http://172.30.1.43:3000";
-//    public static final String HOST = "http://192.168.0.230:3000";
-    public static final String HOST = "http://192.168.0.2:3000";
+//    public static final  String HOST = "http://172.30.1.43:3000"; //민정 집
+//    public static final String HOST = "http://192.168.0.230:3000"; //학원
+//    public static final String HOST = "http://192.168.0.2:3000"; //현진 집
+    public static final String HOST = "http://192.168.0.2:8080"; //현진 집 톰캣
 
     /* 프로그램 실행 후 필요한 상수 값들 정의 시작 */
     private static final int CANCLE_FROM_CONTENT = 0;

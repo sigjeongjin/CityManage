bf3712df6d9d850fb3a99323b1b97a7ff4593c40
minjeong <jadepoint@naver.com>
2017-10-17 19:13:46 +0900

    설정파일 수정

--- app/src/main/java/com/citymanage/member/LoginActivity.java
+++ app/src/main/java/com/citymanage/member/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -19,6 +20,7 @@ import com.citymanage.R;
 import com.citymanage.member.repo.MemberRepo;
 import com.citymanage.member.repo.MemberService;
 import com.common.Module;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import retrofit2.Call;
 import retrofit2.Callback;

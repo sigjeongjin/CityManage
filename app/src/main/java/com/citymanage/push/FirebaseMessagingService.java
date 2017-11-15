c78d25f917b2205ffba6b9135b6664c43bd402c3
KoreanWarrior <bbcjswo@naver.com>
2017-11-15 14:48:08 +0900

    수정중

--- app/src/main/java/com/citymanage/push/FirebaseMessagingService.java
+++ app/src/main/java/com/citymanage/push/FirebaseMessagingService.java
@@ -7,10 +7,18 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.citymanage.MainActivity;
 import com.citymanage.R;
+import com.citymanage.gm.GmInfoActivity;
+import com.citymanage.sm.SmInfoActivity;
+import com.citymanage.tm.TmInfoActivity;
+import com.citymanage.wm.WmInfoActivity;
 import com.google.firebase.messaging.RemoteMessage;
+
+import static com.citymanage.push.PushHistoryActivity.SENSORID;
 
 /**
  * Created by minjeongkim on 2017-10-15.
@@ -30,7 +38,28 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
 
     private void sendNotification(String title, String contents) {
 
+        Log.e("DEBUG ", title);
+        Log.e("DEBUG ", contents);
+
         Intent intent = new Intent(this, MainActivity.class);
+
+        Log.e("DEBUG " , String.valueOf(TextUtils.indexOf(contents,"T")));
+
+        if(TextUtils.indexOf(contents,"T") ==0) {
+            intent = new Intent(this, TmInfoActivity.class);
+        }
+        else if(TextUtils.indexOf(contents,"G") ==0) {
+            intent = new Intent(this, GmInfoActivity.class);
+        }
+        else if(TextUtils.indexOf(contents,"W") ==0) {
+            intent = new Intent(this, WmInfoActivity.class);
+        }
+        else if(TextUtils.indexOf(contents,"S") ==0) {
+            intent = new Intent(this, SmInfoActivity.class);
+        }
+
+        intent.putExtra(SENSORID,contents);
+
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
